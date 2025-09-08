9584c1d17cae100d37c9657762af623e00b3e3cd
Mok2Lee <aszx145263@gmail.com>
2025-09-09 02:05:33 +0900

    readme.txt 파일을 확인하고 파이썬파일을 수정하세요

---
+++ star.py
@@ -0,0 +1,12 @@
+class Pyramid:
+    def __init__(self, height):
+        self.height = height
+
+    def print_star_pyramid(self):
+        for i in range(1, self.height + 1):
+            print(" " * (self.height - i) + "*" * (2 * i - 1))
+
+
+height = int(input("피라미드 높이를 입력하세요: "))
+p = Pyramid(height)
+p.print_star_pyramid()
